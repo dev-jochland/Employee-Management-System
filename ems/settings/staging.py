c41be41904f509edd43c1effd5b4a22df2e705af
Joel Onojason <joel.onojason@outlook.com>
2022-03-29 17:22:50 +0100

    test scripts updated

--- ems/settings/staging.py
+++ ems/settings/staging.py
@@ -1,27 +1,27 @@
 from .base import *
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.mysql',
-#         'NAME': 'test_test',
-#         'USER': 'user_',
-#         'PASSWORD': 'secret_pw',
-#         'HOST': 'db',  # referencing docker db here
-#         'PORT': 3306,
-#         'MYSQL_ALLOW_EMPTY_PASSWORD': 'yes',
-#         'ATOMIC_REQUESTS': True
-#     }
-# }
-
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'ems_db',
-        'USER': 'postgres',
-        'PASSWORD': 'postgres',
-        'HOST': '127.0.0.1',
-        'PORT': 5432,
-        'ATOMIC_REQUESTS': DEBUG
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'test_test',
+        'USER': 'user_',
+        'PASSWORD': 'secret_pw',
+        'HOST': 'db',  # referencing docker db here
+        'PORT': 3306,
+        'MYSQL_ALLOW_EMPTY_PASSWORD': 'yes',
+        'ATOMIC_REQUESTS': True
     }
 }
+
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': 'ems_db',
+#         'USER': 'postgres',
+#         'PASSWORD': 'postgres',
+#         'HOST': '127.0.0.1',
+#         'PORT': 5432,
+#         'ATOMIC_REQUESTS': DEBUG
+#     }
+# }
